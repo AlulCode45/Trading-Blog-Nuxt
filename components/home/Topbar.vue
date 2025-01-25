2a877f82e79fccc091de00c9de9ca849c8abbf16
AlulCode45 <alulgans70@gmail.com>
2025-01-25 21:45:32 +0700

    feat: add Topbar component with navigation links

---
+++ components/home/Topbar.vue
@@ -0,0 +1,43 @@
+<template>
+    <header>
+        <div class="flex justify-between items-center max-w-7xl mx-auto p-4">
+            <div class="logo flex items-center">
+                <img src="/public/favicon.ico" alt="logo" />
+                <h1 class="text-md font-semibold ml-2">ATrade</h1>
+            </div>
+            <nav>
+                <ul class="flex items-center space-x-5">
+                    <li><a href="#" :class="[
+                        'hover:text-blue-500',
+                        'hover:font-bold',
+                        'transition-all',
+                        'hover:border-b-2',
+                        'hover:border-b-blue-500'
+                    ]">Home</a></li>
+                    <li><a href="#" :class="[
+                        'hover:text-blue-500',
+                        'hover:font-bold',
+                        'transition-all',
+                        'hover:border-b-2',
+                        'hover:border-b-blue-500'
+                    ]">Articles</a></li>
+                    <li><a href="#" :class="[
+                        'hover:text-blue-500',
+                        'hover:font-bold',
+                        'transition-all',
+                        'hover:border-b-2',
+                        'hover:border-b-blue-500'
+                    ]">Analysis</a></li>
+                    <li><a href="#" :class="[
+                        'hover:text-blue-500',
+                        'hover:font-bold',
+                        'transition-all',
+                        'hover:border-b-2',
+                        'hover:border-b-blue-500'
+                    ]">Join Member</a></li>
+                    <HomeButton buttonText="Sign In" />
+                </ul>
+            </nav>
+        </div>
+    </header>
+</template>
