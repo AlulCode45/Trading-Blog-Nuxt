9f3df15dd589186b7eca109a94d659fee145c24c
AlulCode45 <alulgans70@gmail.com>
2025-01-25 21:45:07 +0700

    feat: button component for home

---
+++ components/home/Button.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <button :class="[
+            backgroundColor || 'bg-green-500',
+            textColor || 'text-white',
+            'hover:bg-green-700 font-bold py-2 px-4 rounded-full '
+        ]" @click="$emit('handleClick')">
+            {{ buttonText }}
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        buttonText: {
+            type: String,
+            required: true,
+        },
+        backgroundColor: {
+            type: String,
+            default: '',
+        },
+        textColor: {
+            type: String,
+            default: '',
+        },
+    },
+    emits: ['handleClick']
+}
+</script>
+
+<style lang="scss" scoped></style>
