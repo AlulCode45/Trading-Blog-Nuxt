e6265fd1558f6fa81893fe4adcdc7cf545ea5c60
AlulCode45 <alulgans70@gmail.com>
2025-01-25 21:45:22 +0700

    feat: add HeroSection component with marquee text

---
+++ components/home/HeroSection.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="max-w-7xl mx-auto p-4">
+        <h1 class="text-[100px] font-bold">The Blog's</h1>
+        <div class="bg-gray-300 rounded-lg w-full overflow-x-hidden p-2">
+            <NuxtMarquee>
+                <h2 class="text-[90px] whitespace-nowrap">Change Your Mindset On Trading Daily</h2>
+            </NuxtMarquee>
+        </div>
+    </div>
+</template>
