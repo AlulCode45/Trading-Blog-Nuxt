5fdf9e57b4ba676f85b78fecab5e76d75563cb6e
AlulCode45 <alulgans70@gmail.com>
2025-01-26 22:38:23 +0700

    feat: Hero section

--- components/home/HeroSection.vue
+++ components/home/HeroSection.vue
@@ -1,10 +1,21 @@
 <template>
-    <div class="max-w-7xl mx-auto p-4">
-        <h1 class="text-[100px] font-bold">The Blog's</h1>
-        <div class="bg-gray-300 rounded-lg w-full overflow-x-hidden p-2">
-            <NuxtMarquee>
-                <h2 class="text-[90px] whitespace-nowrap">Change Your Mindset On Trading Daily</h2>
-            </NuxtMarquee>
+  <div class="hero_section container max-w-7xl mx-auto p-4 h-screen flex bg-gray-50 absolute top-0 left-1/2 transform -translate-x-1/2 -z-10">
+    <div class="hero_section__content flex gap-10 items-center w-full">
+      <div class="hero_content__left">
+        <h2 class="text-4xl font-bold mb-3">Master the Art of Trading</h2>
+        <p class="mb-5">
+          Expert insights, strategies, and tutorials to help you succeed in the financial markets.
+        </p>
+        <button
+            class="px-7 py-2 text-white bg-black rounded-md hover:bg-gradient-to-br hover:from-black hover:to-green-400 hover:shadow-lg transition-all duration-300 ease-in hover:scale-105 ">
+          Start Learning
+        </button>
+      </div>
+      <div class="hero_content__right w-1/2">
+        <div class="bg-gray-700 rounded-lg w-full h-[350px] p-10 text-white">
+          Trading View Simulation
         </div>
+      </div>
     </div>
+  </div>
 </template>
