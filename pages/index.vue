6d2596d18487909c47c60a64ce514990fcf42ce7
AlulCode45 <alulgans70@gmail.com>
2025-01-27 13:57:32 +0700

    feat: using widget and adding article

--- pages/index.vue
+++ pages/index.vue
@@ -10,5 +10,16 @@ useHead({
 </script>
 
 <template>
-
+  <div class="grid grid-cols-3 container max-w-7xl mx-auto p-4 mb-20 gap-10">
+    <div class="col-span-2">
+      <h2 class="text-3xl font-semibold mb-5">Latest Article</h2>
+      <div class="grid grid-cols-2 gap-5">
+        <HomeListContent v-for="i in 4" :key="i"/>
+      </div>
+    </div>
+    <div class="col-span-1 flex flex-col space-y-5">
+      <WidgetCategory/>
+      <WidgetNewsLetter/>
+    </div>
+  </div>
 </template>
