e0b26c4a395aef5c50469e13977ee272d7c427f3
AlulCode45 <alulgans70@gmail.com>
2025-01-27 13:57:18 +0700

    feat: Newsletter widget

---
+++ components/widget/NewsLetter.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="newsletter_card p-5 border rounded-md ">
+    <h3 class="font-semibold text-lg mb-3">Newsletter</h3>
+    <span>Get trading insights delivered to your inbox</span>
+    <input type="email" class="border p-2 rounded-md w-full my-4" placeholder="Your email address">
+    <button class="bg-black text-white p-3 w-full rounded-md font-semibold">Subscribe</button>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
