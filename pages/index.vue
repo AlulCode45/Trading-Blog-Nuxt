c7eca277570a16543761b3f6eda4c02618946f8b
AlulCode45 <alulgans70@gmail.com>
2025-01-27 11:16:31 +0700

    feat: adding title on home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
-
 definePageMeta({
-  layout: 'home'
+  layout: 'home',
 })
+
+useHead({
+  title: 'Atrade - Platform Belajar Trading',
+})
+
 </script>
 
 <template>
-  
+
 </template>
