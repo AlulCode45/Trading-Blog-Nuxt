ad534fa33effb390e0f5d18cefd84852113159aa
AlulCode45 <alulgans70@gmail.com>
2025-01-27 13:57:45 +0700

    feat: list content component

---
+++ components/home/ListContent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="article_card border rounded-lg" v-for="i in 4" :key="i">
+    <div class="card__header">
+      <div class="container w-full h-52 bg-gray-700 rounded-lg grid place-content-center text-white">
+        Article Image
+      </div>
+    </div>
+    <div class="card__body p-3">
+      <span class="font-semibold text-xl mb-3 block">Understanding Market Volatility</span>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium architect</p>
+      <button class="font-semibold mt-3 flex items-center">Read More
+        <Icon name="uil:arrow-right"/>
+      </button>
+    </div>
+  </div>
+</template>
