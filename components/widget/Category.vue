519171d59efb4caa978b03adbdb7037e832e0273
AlulCode45 <alulgans70@gmail.com>
2025-01-27 13:57:08 +0700

    feat: category widget

---
+++ components/widget/Category.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="category_list border rounded-md p-5">
+    <h3 class="font-semibold text-lg mb-3">Category</h3>
+    <ul class="flex flex-col space-y-3">
+      <li>Stock Trading</li>
+      <li>Cryptocurrency</li>
+      <li>Forex Trading</li>
+      <li>Risk Management</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
