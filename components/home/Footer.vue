53f9390e036175e8e95de79021fb1c76ab06582f
AlulCode45 <alulgans70@gmail.com>
2025-01-27 20:12:32 +0700

    feat: adding footer component

---
+++ components/home/Footer.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <footer class="p-4 py-10 bg-gray-50 gap-5">
+    <div class="container max-w-7xl mx-auto grid grid-cols-5 ">
+      <div class="footer__detail col-span-2">
+        <div class="footer__logo flex items-center">
+          <img src="/public/favicon.ico" alt="logo"/>
+          <h1 class="text-md font-semibold ml-2">ATrade</h1>
+        </div>
+        <p class="mt-4">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi cupiditate exercitationem hic in inventore
+          natus quo reiciendis repellat similique, voluptas! Autem eum incidunt nemo nobis! Nihil, nobis, veniam?
+          Laudantium, totam?
+        </p>
+      </div>
+      <div class="footer__links">
+        <span class="font-bold mb-4 block">Quick Links</span>
+        <ul class="flex flex-col space-y-2">
+          <li>
+            About Us
+          </li>
+          <li>
+            Contact
+          </li>
+          <li>
+            Privacy policy
+          </li>
+          <li>
+            Terms of Service
+          </li>
+        </ul>
+      </div>
+      <div class="footer__resource">
+        <span class="font-bold mb-4 block">Quick Links</span>
+        <ul class="flex flex-col space-y-2">
+          <li>
+            About Us
+          </li>
+          <li>
+            Contact
+          </li>
+          <li>
+            Privacy policy
+          </li>
+          <li>
+            Terms of Service
+          </li>
+        </ul>
+      </div>
+      <div class="footer__social">
+        <span class="font-bold mb-4 block">Quick Links</span>
+        <div class="flex gap-3">
+          <Icon name="uil:twitter"/>
+          <Icon name="uil:linkedin"/>
+          <Icon name="uil:facebook"/>
+          <Icon name="uil:instagram"/>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style scoped lang="scss">
+
+</style>
