3f232ca332f0cb290f7a2b74010a08ab3d5103ae
AlulCode45 <alulgans70@gmail.com>
2025-01-28 17:37:43 +0700

    feat: adding icons uil json

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', 'nuxt-marquee', '@nuxt/icon']
+  modules: ['@nuxtjs/tailwindcss', 'nuxt-marquee', '@nuxt/icon'],
+  icon:{
+    serverBundle: {
+      collections: ['uil']
+    }
+  }
 })
